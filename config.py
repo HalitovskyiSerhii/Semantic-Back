230853d8c348ee1e189d90a48825c0ab25a3fe11
Serhii Halitovskyi <halit.sergey@gmail.com>
2021-02-02 23:21:06 +0200

    Added example.env and change config init

--- config.py
+++ config.py
@@ -3,28 +3,36 @@ import os
 
 api_version = "v1"
 confs = {
-    'PROD':'config.ProductionConfig',
-    'DEV':'config.DevelopmentConfig',
-    'TEST':'config.TestingConfig',
+    'PROD': 'config.ProductionConfig',
+    'DEV': 'config.DevelopmentConfig',
+    'TEST': 'config.TestingConfig',
 }
+
 
 class BaseConfig(object):
     API_VERSION = api_version
+    HOST = os.environ.get('HOST', '0.0.0.0'),
+    PORT = os.environ.get('PORT', 80)
     ELASTIC_HOST = os.environ.get('ELASTIC_HOST', 'localhost')
     ELASTIC_PORT = os.environ.get('ELASTIC_PORT', '9200')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
+    ENV = 'production'
     LOG_LEVEL = logging.INFO
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    ENV = 'development'
     LOG_LEVEL = logging.DEBUG
 
 
 class TestingConfig(BaseConfig):
     DEBUG = True
+    TESTING = True
     LOG_LEVEL = logging.DEBUG
 
 
--- run.py
+++ run.py
@@ -4,7 +4,4 @@ import os
 app = create_app(os.environ.get('ENV', 'DEV'))
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get('HOST', '0.0.0.0'),
-        port=os.environ.get('PORT', 80)
-    )
+    app.run()
